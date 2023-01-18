fbcf1d9a099b29cd27959101bd374bd4c9046937
SaatwikRishi <saatwik_s@mckinsey.com>
2023-01-18 14:44:36 +0530

    feat: Create bowling score calculator

--- bowlingScoreCalculator.js
+++ bowlingScoreCalculator.js
@@ -1,87 +1,64 @@
-const getSum = (frame) => frame.length == 0 ? 0: frame.reduce((acc, item) => acc+item);
+const getSum = (frame) => frame.length == 0 ? 0 : frame.reduce((acc, item) => acc + item);
+/**
+ * 
+ * @param {Array<Integer>} rolls 
+ */
 
 const bowlingScoreCalculator = (rolls) => {
-	if(!Array.isArray(rolls)) throw 'unsupported type';
-	if(rolls.length == 0) throw 'Not enough frames';
-
-    
+	if (!Array.isArray(rolls)) throw 'unsupported type';
+	if (rolls.length == 0) throw 'Not enough frames';
+	let throwsInCurrentFrame = 0;
+	const totalFrames = 10;
 
 	const frames = [];
+	let currentFrameScores = [];
 	let score = 0;
-	let currFrame = new Array();
-	let throwsInThisFrame = 0;
-	let finalCaseIsThere = false;
-    
+	let currentFrame = 1;
 	rolls.forEach((roll, index) => {
-		if(finalCaseIsThere) {
+		if (currentFrame == totalFrames) {
+			currentFrameScores.push(roll);
 			return;
 		}
-		if(throwsInThisFrame == 2) {
-			score += getSum(currFrame);
-			frames.push(currFrame);
-			currFrame = [];
-			throwsInThisFrame = 0;
+
+		if (throwsInCurrentFrame === 2) {
+			score += getSum(currentFrameScores);
+			frames.push(currentFrameScores);
+			currentFrameScores = [];
+			throwsInCurrentFrame = 0;
+
 		}
-		if(index < 19) {
-			throwsInThisFrame+=1;
-			if(roll == 10 && throwsInThisFrame == 1){ // case for strike
-				currFrame.push(10, rolls[index+1], rolls[index+2]);
-				throwsInThisFrame = 2;
-				if(index+2 == rolls.length-1) {
-					finalCaseIsThere = true;
-					frames.push(currFrame);
-					score+= getSum(currFrame);
-					currFrame = [];   
-				}
-			}
-			else if(getSum(currFrame) + roll == 10 && throwsInThisFrame == 2) { // case for spare
-                 
-				currFrame.push(roll, rolls[index+1]);
-				throwsInThisFrame = 2;
-                
-				if(index+1 == rolls.length-1) {
-					finalCaseIsThere = true;
-					frames.push(currFrame);
-					score+= getSum(currFrame);
-					currFrame = [];   
-				}
-			}
-			else {
-				currFrame.push(roll);
-			}
-    
+
+		if (roll === 10) { // case for strike
+			currentFrameScores.push(10, rolls[index], rolls[index + 1]);
+			throwsInCurrentFrame = 2;
 		}
-		else {
-			for(let currRoll = 19; currRoll < rolls.length; currRoll++) currFrame.push(rolls[currRoll]);
-			finalCaseIsThere = true;
-			frames.push(currFrame);
-			score+= getSum(currFrame);
-			currFrame = [];
-
-			// last case
+		else if (getSum(currentFrameScores) + roll == 10) { //case for spare
+			currentFrameScores.push(roll, rolls[index + 1]);
+			throwsInCurrentFrame = 2;
 		}
-        
+		else { // case for non 10s
+			currentFrameScores.push(roll);
+			throwsInCurrentFrame++;
+		}
+		if(throwsInCurrentFrame == 2) {
+			currentFrame++;
 
+		}
 
-
-	} );    
-	if(currFrame.length != 0) {
-		frames.push(currFrame);
-		score+= getSum(currFrame);
+	});
+	if (currentFrameScores.length != 0) {
+		score += getSum(currentFrameScores);
+		frames.push(currentFrameScores);
 	}
+	console.log(score);
+	console.log(frames);
 
-	const finalObj = {score : score, frames: {}};
-
-	frames.forEach((frame, index ) =>{
-		finalObj.frames[index+1] = frame;} );
-	
-
-	return finalObj;
 
 };
 
+module.exports = { bowlingScoreCalculator };
 
-console.log(bowlingScoreCalculator([  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 10, 10]));
-
-bowlingScoreCalculator([3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6]);
-bowlingScoreCalculator([6, 4, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+console.log(bowlingScoreCalculator([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 10, 10]));
+console.log(bowlingScoreCalculator([3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6]));
+console.log(bowlingScoreCalculator([6, 4, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]));
+console.log(bowlingScoreCalculator([10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10]));
--- bowlingScoreCalculator.test.js
+++ bowlingScoreCalculator.test.js
@@ -1,11 +1,24 @@
-const {describe, test, it} = require('@jest/globals');
+
+const { describe, it, expect } = require('@jest/globals');
+const { bowlingScoreCalculator } = require('./bowlingScoreCalculator');
 
 describe('Bowling Score Calculator', () => {
-    it('Should give error when no array is provided'.  () => {
 
-        test('no array', () => {
-            bowlingScoreCalculator('123');
-        }).toThrow('unsupported type');
-    });
+	it('Should throw an error when no array is provided', () => {
+		expect(() => {
+			bowlingScoreCalculator('123');
 
+		}).toThrow('unsupported type');
+
+
+
+	});
+	it('Should throw an error when the length of array is 0', () => {
+		expect(() => {
+			bowlingScoreCalculator([]);
+		}).toThrow('Not enough frames');
+	});
+
+    
 });
+
