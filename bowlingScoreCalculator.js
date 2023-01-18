7f8e893c1b0f4dd407a29ea97edc213f95257f38
SaatwikRishi <saatwik_s@mckinsey.com>
2023-01-18 12:53:33 +0530

    fix: Apply linting

--- bowlingScoreCalculator.js
+++ bowlingScoreCalculator.js
@@ -1,82 +1,82 @@
 const getSum = (frame) => frame.length == 0 ? 0: frame.reduce((acc, item) => acc+item);
 
 const bowlingScoreCalculator = (rolls) => {
-    if(!Array.isArray(rolls)) throw 'unsupported type';
-    if(rolls.length == 0) throw 'Not enough frames';
+	if(!Array.isArray(rolls)) throw 'unsupported type';
+	if(rolls.length == 0) throw 'Not enough frames';
 
     
 
-    const frames = [];
-    let score = 0;
-    let currFrame = new Array();
-    let throwsInThisFrame = 0;
-    let finalCaseIsThere = false;
+	const frames = [];
+	let score = 0;
+	let currFrame = new Array();
+	let throwsInThisFrame = 0;
+	let finalCaseIsThere = false;
     
-    rolls.forEach((roll, index) => {
-        if(finalCaseIsThere) {
-            return;
-        }
-        if(throwsInThisFrame == 2) {
-            score += getSum(currFrame);
-            frames.push(currFrame);
-            currFrame = [];
-            throwsInThisFrame = 0;
-        }
-        if(index < 19) {
-            throwsInThisFrame+=1;
-            if(roll == 10 && throwsInThisFrame == 1){ // case for strike
-                currFrame.push(10, rolls[index+1], rolls[index+2]);
-                throwsInThisFrame = 2;
-                if(index+2 == rolls.length-1) {
-                    finalCaseIsThere = true;
-                    frames.push(currFrame);
-                    score+= getSum(currFrame);
-                    currFrame = [];   
-                }
-            }
-            else if(getSum(currFrame) + roll == 10 && throwsInThisFrame == 2) { // case for spare
+	rolls.forEach((roll, index) => {
+		if(finalCaseIsThere) {
+			return;
+		}
+		if(throwsInThisFrame == 2) {
+			score += getSum(currFrame);
+			frames.push(currFrame);
+			currFrame = [];
+			throwsInThisFrame = 0;
+		}
+		if(index < 19) {
+			throwsInThisFrame+=1;
+			if(roll == 10 && throwsInThisFrame == 1){ // case for strike
+				currFrame.push(10, rolls[index+1], rolls[index+2]);
+				throwsInThisFrame = 2;
+				if(index+2 == rolls.length-1) {
+					finalCaseIsThere = true;
+					frames.push(currFrame);
+					score+= getSum(currFrame);
+					currFrame = [];   
+				}
+			}
+			else if(getSum(currFrame) + roll == 10 && throwsInThisFrame == 2) { // case for spare
                  
-                currFrame.push(roll, rolls[index+1]);
-                throwsInThisFrame = 2
+				currFrame.push(roll, rolls[index+1]);
+				throwsInThisFrame = 2;
                 
-                if(index+1 == rolls.length-1) {
-                    finalCaseIsThere = true;
-                    frames.push(currFrame);
-                    score+= getSum(currFrame);
-                    currFrame = [];   
-                }
-            }
-            else {
-                currFrame.push(roll);
-            }
+				if(index+1 == rolls.length-1) {
+					finalCaseIsThere = true;
+					frames.push(currFrame);
+					score+= getSum(currFrame);
+					currFrame = [];   
+				}
+			}
+			else {
+				currFrame.push(roll);
+			}
     
-        }
-        else {
-           for(let currRoll = 19; currRoll < rolls.length; currRoll++) currFrame.push(rolls[currRoll]);
-           finalCaseIsThere = true;
-           frames.push(currFrame);
-           score+= getSum(currFrame);
-           currFrame = [];
+		}
+		else {
+			for(let currRoll = 19; currRoll < rolls.length; currRoll++) currFrame.push(rolls[currRoll]);
+			finalCaseIsThere = true;
+			frames.push(currFrame);
+			score+= getSum(currFrame);
+			currFrame = [];
 
-            // last case
-        }
+			// last case
+		}
         
 
 
 
-    } )    
-    if(currFrame.length != 0) {
-        frames.push(currFrame);
-        score+= getSum(currFrame);
-    }
+	} );    
+	if(currFrame.length != 0) {
+		frames.push(currFrame);
+		score+= getSum(currFrame);
+	}
 
-    console.log(frames);
-    console.log(score);
+	console.log(frames);
+	console.log(score);
 
-}
+};
 
 
-bowlingScoreCalculator([  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 10, 10])
+bowlingScoreCalculator([  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 10, 10]);
 
-bowlingScoreCalculator([3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6])
-bowlingScoreCalculator([6, 4, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+bowlingScoreCalculator([3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6]);
+bowlingScoreCalculator([6, 4, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
